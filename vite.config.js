a3f849d1be2138c9ab1e0744d81ab4d3ba11b952
Nitin <nitinchoudhary22112004@gmail.com>
2024-08-21 19:49:03 +0530

    Updated Vite Config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), nodePolyfills()],
-  resolve: {
-    alias: {
-      // '@': path.resolve(__dirname, './src'), // <-- This line configures the @ alias
-    },
-  },
+  plugins: [react(),],
 })
